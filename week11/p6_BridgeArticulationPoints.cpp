50adeb96ba680df38305a5ee0a30082fb7d3a79f
tandat17z <letandatdowny2003@gmail.com>
2023-12-06 12:52:43 +0700

    add week11

--- week11/p6_BridgeArticulationPoints.cpp
+++ week11/p6_BridgeArticulationPoints.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 10000;
+const int N = 50000;
 int n, m;
 vector<int> A[N];
 bool visited[N];
@@ -10,6 +10,8 @@ int low[N];
 
 int t;
 vector<pair<int, int>> bridges;
+unordered_set<int> aPoints;
+int root;
 
 void input(){
     ios_base::sync_with_stdio(0);
@@ -29,19 +31,25 @@ void dfs(int s, int ps){
     num[s] = t;
     low[s] = num[s];
     visited[s] = true;
+    int cnt = 0;
     for( int i = 0; i< A[s].size(); i++){
         int v = A[s][i];
-        if( v == ps) continue;
-        if( visited[v] ){
+        if( v == ps) continue; // bỏ qua cha ps của v
+        if( visited[v] ){ // back edge là sv
             low[s] = min(low[s], num[v]);
         }else{
+            cnt += 1;
             dfs(v, s);
             low[s] = min(low[s], low[v]);
             if( low[v] > num[s]){
                 bridges.push_back(make_pair(s, v));
             }
+            if( low[v] >= num[s] && ps != -1){
+                aPoints.insert(s);
+            }
         }
     }
+    if( cnt > 1 && ps == -1) aPoints.insert(s);
 }
 
 void init(){
@@ -51,12 +59,20 @@ void solve(){
     init();
     t = 0;
     for( int s = 1; s <= n; s++){
-        if( !visited[s]) dfs(s, -1);
+        if( !visited[s]) {
+            root = s;
+            dfs(s, -1);
+        }
     }
-    cout << "bridges = ";
-    for( int i = 0; i < bridges.size(); i++){
-        cout << "(" << bridges[i].first << ", " << bridges[i].second << ")";
-    }
+    // cout << "bridges = ";
+    // for( int i = 0; i < bridges.size(); i++){
+    //     cout << "(" << bridges[i].first << ", " << bridges[i].second << ")";
+    // }
+    // for( auto i : aPoints){
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+    printf("%d %d", aPoints.size(), bridges.size());
 }
 int main(){
     freopen("input/data6.txt", "r", stdin);
